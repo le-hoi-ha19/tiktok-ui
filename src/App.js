b104164a3fa7fd61b01779ad72fa3d737158b3c8
le-hoi-ha19 <hale85528@gmail.com>
2023-07-22 06:43:02 +0700

    add customize-cra and babel-plug-module-resolver

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+
+import '~/App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>hà đẹp trai quá</h1>
+      <h1 className="h">hà đẹp trai quá</h1>
     </div>
   );
 }
