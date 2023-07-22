ecf9618086d268943424af399ed16c55556b2a62
le-hoi-ha19 <hale85528@gmail.com>
2023-07-22 07:07:18 +0700

    set up prettier

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-
 import '~/App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="h">hà đẹp trai quá</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1 className="h">hà đẹp trai quá</h1>
+        </div>
+    );
 }
 
 export default App;
