10a4febf4a9095d6755b81978ac6d5dbb86c5e2d
le-hoi-ha19 <hale85528@gmail.com>
2023-08-21 06:24:28 +0700

    Xây dựng UI phần sidebar #3

--- src/components/SuggestedAccounts/AccountItem.js
+++ src/components/SuggestedAccounts/AccountItem.js
@@ -1,22 +1,36 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import Tippy from '@tippyjs/react/headless';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 
+import { Wrapper as PopperWrapper } from '~/components/Popper';
 import styles from './SuggestedAccounts.module.scss';
+import AccountPreview from './AccountPreview';
 
 const cx = classNames.bind(styles);
 
 function AccountItem() {
+    // hàm hiển thị lên cái pop up khi hover vào chỗ tài khoản được đề xuất
+    const renderPreview = (props) => {
+        return (
+            // tabIndex =-1 là khi bấm phím tab thì k cho focus vào phần tử
+            <div className={cx('preview')} tabIndex="-1" {...props}>
+                <PopperWrapper>
+                    <AccountPreview />
+                </PopperWrapper>
+            </div>
+        );
+    };
     return (
-       <div>
-            <Tippy interactive delay={[800, 0]} placement='bottom' render={() => <h1> Account preview</h1>}>
+        <div>
+            {/* render = hàm hiện lên pop up */}
+            <Tippy  interactive delay={[800, 0]} offset={[-20,0]} placement="bottom" render={renderPreview}>
                 <div className={cx('account-item')}>
                     <img
                         className={cx('avatar')}
                         src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/42a81079b5885e152707b170d63ba2df~c5_100x100.jpeg?x-expires=1690765200&x-signature=yg9h540VDd5WMtVkaE8ItoNBeCc%3D"
-                        alt=""
+                        alt="avatar"
                     />
                     <div className={cx('item-info')}>
                         <p className={cx('nickname')}>
@@ -27,7 +41,7 @@ function AccountItem() {
                     </div>
                 </div>
             </Tippy>
-       </div>
+        </div>
     );
 }
 
---
+++ src/components/SuggestedAccounts/AccountPreview/AccountPreview.js
@@ -0,0 +1,39 @@
+import classNames from 'classnames/bind';
+import styles from './AccountPreview.module.scss';
+import Button from '~/components/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
+function AccountPreview() {
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('header')}>
+                <img
+                    className={cx('avatar')}
+                    src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/42a81079b5885e152707b170d63ba2df~c5_100x100.jpeg?x-expires=1690765200&x-signature=yg9h540VDd5WMtVkaE8ItoNBeCc%3D"
+                    alt=""
+                />
+                <div>
+                    <Button primary className={cx('follow-btn')}> follow</Button>
+                </div>
+            </div>
+            <div className={cx('body')}>
+                <p className={cx('nickname')}>
+                    <strong>lehoiha</strong>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </p>
+                <p className={cx('name')}>Lê hội hà</p>
+                <p className={cx('analytics')}>
+                    <strong className={cx('value')}>8.2M </strong>
+                    <span className={cx('label')}>Follower</span>
+                    <strong className={cx('value')}>8.2M </strong>
+                    <span className={cx('label')}>Likes</span>
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default AccountPreview;
